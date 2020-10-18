16ba450c60a58947c1b3987f28c1101ab027aa6f
Om Sinha <omsinha017@gmail.com>
2020-10-19 02:11:54 +0530

    Update Production for Allowed_host

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(8prjl+9t+%fr#&uqr*i!*q8a7-qg$u-9yee#7z498nev+-1h3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
