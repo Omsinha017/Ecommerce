d60e96e611ab2a5beb368a4d92558b41b457f4f1
Om Sinha <omsinha017@gmail.com>
2020-10-23 23:06:18 +0530

    Working

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -11,7 +11,7 @@ AWS_STORAGE_BUCKET_NAME = 'ecommerce-om'
 S3DIRECT_REGION = 'us-west-2'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
+MEDIA_ROOT = S3_URL
 STATIC_URL = S3_URL + 'static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
